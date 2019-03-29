1054a2d426ac182f9aa7f00d5fa4a448a6cea133
Ricardo Gómez <jgomez@oasishoteles.com>
2019-03-29 14:59:16 -0500

    react styled components agregado

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,62 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import Axios from 'axios';
+import localforage from 'localforage';
+
+const API_ENPOINT = 'https://pokeapi.co/api/v2/pokemon/?limit=150'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePokemon: {
+        id: null,
+        name: null,
+      },
+      loading: true,
+      pokemons: [],
+    };
+  }
+
+  componentDidMount() {
+    localforage.getItem('pokemonList')
+      .then((p) => {
+        const isCached = Boolean(p);
+        if (!isCached) {
+          Axios.get(API_ENPOINT)
+            .then((r) => {
+              const pokemons = r.data;
+              localforage.setItem('pokemonList', pokemons.results);
+              this.setState({
+                pokemons: pokemons.results,
+                loading: false,
+              });
+            });
+        } else {
+          this.setState({
+            pokemons: p,
+            loading: false,
+          });
+        }
+      });
+  }
+
+  setActivePokemon(pokemonObj) {
+    this.setState({
+      activePokemon: pokemonObj,
+    });
+  }
+
   render() {
-    console.log('hola');
+    const { activePokemon, pokemons } = this.state;
+
     return (
       <div>
-        hola
+        {/* <Master active={activePokemon} />
+        <Slave /> */}
+        {pokemons.map(v => (<p key={v.name}>{v.name}</p>))}
       </div>
     );
   }
